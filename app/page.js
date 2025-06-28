be85bdd87a88fb163996cdbb0b91ca2c5f6fca6d
ceeazer1 <ceeazer111@gmail.com>
2025-06-28 14:19:18 -0400

    Fix centering of feature tiles in What Makes This Different section
    - Changed grid from lg:grid-cols-4 to lg:grid-cols-3 to accommodate 3 features
    - Added max-w-5xl mx-auto to center the grid container
    - Features now properly centered on all screen sizes
    - Responsive layout: 1 column on mobile, 2 on tablet, 3 on desktop

--- app/page.js
+++ app/page.js
@@ -213,7 +213,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
             {features.map((feature, index) => (
               <div
                 key={index}
